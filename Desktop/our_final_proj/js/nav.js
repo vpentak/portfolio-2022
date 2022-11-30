5bbcc1aed61cdfe95d071a542cddaf759352cfb8
sasha177 <32937070+sasha177@users.noreply.github.com>
2022-11-30 05:32:25 -0500

    Delete Desktop/our_final_proj directory

--- Desktop/our_final_proj/js/nav.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-    $('a').each(function(){
-        if ($(this).prop('href') == window.location.href) {
-            $(this).addClass('active'); $(this).parents('li').addClass('active');
-        }
-    });
-});
